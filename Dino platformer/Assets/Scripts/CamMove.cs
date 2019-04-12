2a58b410bf4e499fdfb6583a60b1a38f2dc552fb
Roman <mailo91@yandex.ru>
2019-04-12 20:22:53 +0300

    Testing
    Testing the way that git works)

---
+++ Dino platformer/Assets/Scripts/CamMove.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class CamMove : MonoBehaviour {
+    public GameObject Player;
+
+    private void Update()
+    {
+        transform.position = new Vector3 (Player.transform.position.x,Player.transform.position.y, -10f);
+    }
+}
---
+++ Dino platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+
+
+    public float speed = 25f;
+    private Rigidbody2D rb;
+    bool faceRight = true;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float moveX = Input.GetAxis("Horizontal");
+        rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector2.up * 4000);
+
+
+
+        if (moveX > 0 && !faceRight)
+            flip();
+        else if (moveX < 0 && faceRight)
+            flip();
+    }
+    void flip()
+    {
+     faceRight = !faceRight;
+     transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+}
